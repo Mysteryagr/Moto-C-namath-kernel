ea3c8a5b6d3c720fc2aae86446ceddc80951eb7a
Mysteryagr <ahmedrahmy842@gmail.com>
2017-11-27 01:45:43 +0200

    Add build script

---
+++ Build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Mysteryagr
+#Compile kernel with a build script to make things simple
+
+mkdir -p out
+
+export USE_CCACHE=1
+
+export ARCH=arm ARCH_MTK_PLATFORM=mt6735
+
+#Defconfig for Moto C
+make -C $PWD O=$PWD/out ARCH=arm wt6737m_35_n_defconfig
+#make ARCH=arm wt6737m_35_n_defconfig
+
+make -j4 -C $PWD O=$PWD/out ARCH=arm
+#make -j4 ARCH=arm
