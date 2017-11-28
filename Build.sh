24d5855e7d51dc5cd74024afad119b1f171a4db4
Mysteryagr <ahmedrahmy842@gmail.com>
2017-11-28 20:00:28 +0200

    Build.sh: Export User & Host names

--- Build.sh
+++ Build.sh
@@ -9,6 +9,9 @@ export USE_CCACHE=1
 
 export ARCH=arm ARCH_MTK_PLATFORM=mt6735
 
+export KBUILD_BUILD_USER="Mysteryagr"
+export KBUILD_BUILD_HOST="TravisCI"
+
 #Defconfig for Moto C
 make -C $PWD O=$PWD/out ARCH=arm wt6737m_35_n_defconfig
 #make ARCH=arm wt6737m_35_n_defconfig
