793548884cfebabbf996a883114fa878e68779ce
Ahmed Rahmy <ahmedrahmy842@gmail.com>
2017-12-10 16:30:39 +0200

    Update build script

--- Build.sh
+++ Build.sh
@@ -3,15 +3,18 @@
 #Mysteryagr
 #Compile kernel with a build script to make things simple
 
+export KBUILD_BUILD_USER="A.G.R"
+export KBUILD_BUILD_HOST="Mysteryagr"
+
 mkdir -p out
 
 export USE_CCACHE=1
 
-export ARCH=arm ARCH_MTK_PLATFORM=mt6735
+export ARCH=arm64 ARCH_MTK_PLATFORM=mt6735
 
 #Defconfig for Moto C
-make -C $PWD O=$PWD/out ARCH=arm wt6737m_35_n_defconfig
-#make ARCH=arm wt6737m_35_n_defconfig
+make -C $PWD O=$PWD/out ARCH=arm64 wt6737m_65_n_defconfig
+#make ARCH=arm64 wt6737m65_n_defconfig
 
-make -j4 -C $PWD O=$PWD/out ARCH=arm
-#make -j4 ARCH=arm
+make -j4 -C $PWD O=$PWD/out ARCH=arm64
+#make -j4 ARCH=arm64
